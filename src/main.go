dfbd0f3c835751fa752ccbc3703617846e958237
Kiyo510 <kanzenziai_r@ichirou.co.jp>
2022-08-20 23:53:03 +0900

    Update: go practice, about Emun

--- src/main.go
+++ src/main.go
@@ -2,14 +2,34 @@ package main
 
 import "fmt"
 
-func main() {
-	pow := make([]int, 10)
-	for i := range pow {
-		pow[i] = 1 << uint(i) // == 2**i
+type Season int
+
+const (
+	Spring Season = iota + 1
+	Summer
+	Autumn
+	Winter
+)
+
+func (s Season) String() string {
+	seasons := [...]string{"string", "summer", "autumn", "winter"}
+	if s < Spring || s > Winter {
+		return fmt.Sprintf("Season(%d)", int(s))
 	}
-	
-	fmt.Println(pow)
-	for _, value := range pow {
-		fmt.Printf("%d\n", value)
+	return seasons[s-1]
+}
+
+func (s Season) IsValid() bool {
+	switch s {
+	case Spring, Summer, Autumn, Winter:
+		return true
+	}
+	return false
+}
+
+func main() {
+	mySeasons := []Season{Spring, Summer, Autumn, Winter, 6}
+	for _, s := range mySeasons {
+		fmt.Printf("season: %s, is valid: %t\n", s, s.IsValid())
 	}
 }
