7e999c856a95d14abb7d90e85aa1ffb9c109990d
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2023-03-04 22:52:57 +0900

    study about interface etc

--- src/main.go
+++ src/main.go
@@ -2,14 +2,20 @@ package main
 
 import "fmt"
 
+type Point struct {
+	A int
+	B string
+}
+
+func (p *Point) String() string {
+	return fmt.Sprintf("<<%v, %v>>", p.A, p.B)
+
+}
+
 func main() {
-	sl3 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	fmt.Println(sl3)
+	p := &Point{
+		A: 100, B: "hoge",
+	}
 
-	sl3 = append(sl3, 300)
-	fmt.Println(sl3)
-
-	sl2 := make([]int, 10)
-	fmt.Println(sl2)
-
+	fmt.Println(p)
 }
