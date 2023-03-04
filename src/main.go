e88d4db191037f6367b14826049cf1d798066b10
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2023-03-05 02:08:12 +0900

    study about sort etc

--- src/main.go
+++ src/main.go
@@ -1,21 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
-type Point struct {
-	A int
-	B string
+type Entry struct {
+	Name  string
+	Value int
 }
 
-func (p *Point) String() string {
-	return fmt.Sprintf("<<%v, %v>>", p.A, p.B)
+type List []Entry
 
+func (l List) Len() int {
+	return len(l)
+}
+
+func (l List) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
+
+func (l List) Less(i, j int) bool {
+	if l[i].Value == l[j].Value {
+		return l[i].Name < l[j].Name
+	} else {
+		return l[i].Value < l[j].Value
+	}
 }
 
 func main() {
-	p := &Point{
-		A: 100, B: "hoge",
-	}
+	m := map[string]int{"S": 1, "A": 3, "I": 3, "K": 9, "O": 8}
 
-	fmt.Println(p)
+	lt := List{}
+	for k, v := range m {
+		e := Entry{k, v}
+		lt = append(lt, e)
+	}
+	sort.Sort(lt)
+	fmt.Println(lt)
 }
