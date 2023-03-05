2d982173940284d293eaa0568fb70348d4ba4eb0
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2023-03-05 18:31:38 +0900

    study db connection with mysql

--- src/main.go
+++ src/main.go
@@ -1,24 +1,68 @@
 package main
 
 import (
-	"html/template"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
-	"net/http"
 )
 
-func top(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("tmpl.html")
-	if err != nil {
-		log.Println(err)
-	}
-
-	err = t.Execute(w, "Hello World111!")
-	if err != nil {
-		log.Println(err)
-	}
-}
+var Db *sql.DB
+var err error
 
 func main() {
-	http.HandleFunc("/top", top)
-	http.ListenAndServe(":8080", nil)
+	Db, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:4306)/go_practice?charset=utf8mb4")
+	if err != nil {
+		fmt.Println("MySQL connection ERROR!!")
+		fmt.Println(err)
+	}
+
+	err = Db.Ping()
+	if err != nil {
+		log.Println("connetion refused!!")
+		log.Fatal(err)
+	} else {
+		fmt.Println("connection success!!")
+	}
+
+	defer Db.Close()
+
+	cmd := `insert into persons (name, age) values (?,?)`
+	_, err := Db.Exec(cmd, "Tanaka", 49)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("insert success")
+
+	//p := Person{}
+	//cmd := `SELECT * FROM persons WHERE name = ?`
+	//row := Db.QueryRow(cmd, "sano")
+	//err := row.Scan(&p.Name, &p.Age)
+	//if err != nil {
+	//	if err == sql.ErrNoRows {
+	//		log.Println("No row")
+	//	} else {
+	//		log.Println(err)
+	//	}
+	//}
+	//fmt.Println(p.Name, p.Age)
+
+	//cmd := `select * from persons`
+	//rows, _ := Db.Query(cmd)
+	//defer rows.Close()
+	//var pp []Person
+	//
+	//for rows.Next() {
+	//	var p Person
+	//	err := rows.Scan(&p.Name, &p.Age)
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//
+	//	pp = append(pp, p)
+	//}
+	//
+	//for _, person := range pp {
+	//	fmt.Println(person.Name, person.Age)
+	//}
 }
