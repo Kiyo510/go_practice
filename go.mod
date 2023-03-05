b4b246a765153db2737183c9af72346b9d1e6621
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2023-03-05 18:46:36 +0900

    study execute query with mysql

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module go-practice
 go 1.19
 
 require github.com/go-sql-driver/mysql v1.7.0
+
+require gopkg.in/go-ini/ini.v1 v1.67.0 // indirect
--- src/main.go
+++ src/main.go
@@ -10,6 +10,12 @@ import (
 var Db *sql.DB
 var err error
 
+type Person struct {
+	Id   int
+	Name string
+	Age  int
+}
+
 func main() {
 	Db, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:4306)/go_practice?charset=utf8mb4")
 	if err != nil {
@@ -27,17 +33,9 @@ func main() {
 
 	defer Db.Close()
 
-	cmd := `insert into persons (name, age) values (?,?)`
-	_, err := Db.Exec(cmd, "Tanaka", 49)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("insert success")
-
-	//p := Person{}
 	//cmd := `SELECT * FROM persons WHERE name = ?`
-	//row := Db.QueryRow(cmd, "sano")
-	//err := row.Scan(&p.Name, &p.Age)
+	//row := Db.QueryRow(cmd, "hiko")
+	//err = row.Scan(&p.Name, &p.Age)
 	//if err != nil {
 	//	if err == sql.ErrNoRows {
 	//		log.Println("No row")
@@ -47,22 +45,22 @@ func main() {
 	//}
 	//fmt.Println(p.Name, p.Age)
 
-	//cmd := `select * from persons`
-	//rows, _ := Db.Query(cmd)
-	//defer rows.Close()
-	//var pp []Person
-	//
-	//for rows.Next() {
-	//	var p Person
-	//	err := rows.Scan(&p.Name, &p.Age)
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-	//
-	//	pp = append(pp, p)
-	//}
-	//
-	//for _, person := range pp {
-	//	fmt.Println(person.Name, person.Age)
-	//}
+	cmd := `select * from persons`
+	rows, _ := Db.Query(cmd)
+	defer rows.Close()
+	var pp []Person
+
+	for rows.Next() {
+		var p Person
+		err := rows.Scan(&p.Id, &p.Name, &p.Age)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		pp = append(pp, p)
+	}
+
+	for _, person := range pp {
+		fmt.Println(person.Id, person.Name, person.Age)
+	}
 }
