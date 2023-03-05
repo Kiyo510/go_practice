10e971b19533316b3ae3eeb183cf39c524a1c84f
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2023-03-05 19:35:45 +0900

    fix variable

--- utils/logging.go
+++ utils/logging.go
@@ -7,13 +7,12 @@ import (
 )
 
 func LoggingSettings(logFile string) {
-	var err error
-	logFile, err = os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	logfile, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	multiLogFile := io.MultiWriter(os.Stdout, logFile)
+	multiLogFile := io.MultiWriter(os.Stdout, logfile)
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	log.SetOutput(multiLogFile)
 }
