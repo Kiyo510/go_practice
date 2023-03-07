548291d14ce6f3cf5911384b109e76c7ffa76220
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2023-03-08 04:04:30 +0900

    study about generics settypes

--- src/generics.go
+++ src/generics.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 type Number interface {
-	int | int32 | int64 | float32 | float64
+	~int | ~int32 | ~int64 | ~float32 | ~float64
 }
+
+type MyInt int
 
 func Max[T Number](x, y T) T {
 	if x >= y {
@@ -14,6 +16,7 @@ func Max[T Number](x, y T) T {
 }
 
 func main() {
-	fmt.Println(Max(1, 2))
+	var x, y MyInt = 1, 2
+	fmt.Println(Max(x, y))
 	fmt.Println(Max(1.2201, 1.220))
 }
